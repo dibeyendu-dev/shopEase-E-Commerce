6a69c3f416e1cdb17b1c051c88a2e06b2082b9d3
Dibeyendu Maity <dibeyendumaity@gmail.com>
2026-07-30 12:05:17 +0530

    Small backtop button added

--- js/script.js
+++ js/script.js
@@ -4,3 +4,19 @@ const navbar = document.querySelector(".navbar");
 menuBtn.addEventListener("click", () => {
     navbar.classList.toggle("active");
 });
+const backToTop=document.getElementById("backToTop");
+
+window.addEventListener("scroll",function(){
+    if(window.scrollY>300){
+        backToTop.classList.add("show");
+    }else{
+        backToTop.classList.remove("show");
+    }
+});
+
+backToTop.addEventListener("click",function(){
+    window.scrollTo({
+        top:0,
+        behavior:"smooth"
+    });
+});
