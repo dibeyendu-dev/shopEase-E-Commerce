8248d7f3a8634a6296471d9d4eb5b2825fffdd12
Dibeyendu Maity <dibeyendumaity@gmail.com>
2026-08-14 21:32:26 +0530

    login js part 2 added

--- js/login.js
+++ js/login.js
@@ -9,3 +9,30 @@ const email=document.getElementById("email");
 const password=document.getElementById("password");
 
 const togglePassword=document.getElementById("togglePassword");
+// ==========================================
+// SHOW / HIDE PASSWORD
+// ==========================================
+
+togglePassword.addEventListener("click",function(){
+
+if(password.type==="password"){
+
+password.type="text";
+
+this.classList.remove("fa-eye");
+
+this.classList.add("fa-eye-slash");
+
+}
+
+else{
+
+password.type="password";
+
+this.classList.remove("fa-eye-slash");
+
+this.classList.add("fa-eye");
+
+}
+
+});
