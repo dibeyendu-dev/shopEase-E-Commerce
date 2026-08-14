b06de66b606244b00b28e3f7e745827970235529
Dibeyendu Maity <dibeyendumaity@gmail.com>
2026-08-14 21:25:28 +0530

    login js part 1 added

---
+++ js/login.js
@@ -0,0 +1,11 @@
+// ==========================================
+// SELECT ELEMENTS
+// ==========================================
+
+const loginForm=document.getElementById("loginForm");
+
+const email=document.getElementById("email");
+
+const password=document.getElementById("password");
+
+const togglePassword=document.getElementById("togglePassword");
