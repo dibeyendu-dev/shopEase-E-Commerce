a96bcc6db8d32fc73612c1dd59cb8aa3ce70ca4f
Dibeyendu Maity <dibeyendumaity@gmail.com>
2026-08-14 21:55:13 +0530

    register js part 2 added

--- js/register.js
+++ js/register.js
@@ -15,3 +15,58 @@ const confirmPassword = document.getElementById("confirmPassword");
 const togglePassword = document.getElementById("togglePassword");
 
 const toggleConfirmPassword = document.getElementById("toggleConfirmPassword");
+// ==========================================
+// SHOW / HIDE PASSWORD
+// ==========================================
+
+togglePassword.addEventListener("click",function(){
+
+if(password.type==="password"){
+
+password.type="text";
+
+this.classList.remove("fa-eye");
+
+this.classList.add("fa-eye-slash");
+
+}
+
+else{
+
+password.type="password";
+
+this.classList.remove("fa-eye-slash");
+
+this.classList.add("fa-eye");
+
+}
+
+});
+
+// ==========================================
+// SHOW / HIDE CONFIRM PASSWORD
+// ==========================================
+
+toggleConfirmPassword.addEventListener("click",function(){
+
+if(confirmPassword.type==="password"){
+
+confirmPassword.type="text";
+
+this.classList.remove("fa-eye");
+
+this.classList.add("fa-eye-slash");
+
+}
+
+else{
+
+confirmPassword.type="password";
+
+this.classList.remove("fa-eye-slash");
+
+this.classList.add("fa-eye");
+
+}
+
+});
