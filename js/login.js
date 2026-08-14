82e21115423fa187f3ef2ed4464be25dc1e0972f
Dibeyendu Maity <dibeyendumaity@gmail.com>
2026-08-14 21:33:38 +0530

    login js part 3 added

--- js/login.js
+++ js/login.js
@@ -36,3 +36,55 @@ this.classList.add("fa-eye");
 }
 
 });
+// ==========================================
+// LOGIN FORM
+// ==========================================
+
+loginForm.addEventListener("submit",function(event){
+
+event.preventDefault();
+
+const userEmail=email.value.trim();
+
+const userPassword=password.value.trim();
+
+if(userEmail==="" || userPassword===""){
+
+alert("Please fill all fields.");
+
+return;
+
+}
+
+const emailPattern=/^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+if(!emailPattern.test(userEmail)){
+
+alert("Please enter a valid email.");
+
+return;
+
+}
+
+if(userPassword.length<6){
+
+alert("Password must be at least 6 characters.");
+
+return;
+
+}
+
+const user={
+
+email:userEmail,
+password:userPassword
+
+};
+
+localStorage.setItem("currentUser",JSON.stringify(user));
+
+alert("Login Successful 🎉");
+
+window.location.href="index.html";
+
+});
