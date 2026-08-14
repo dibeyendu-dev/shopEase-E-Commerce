fb510f73c1319e1be4afd313cd98d755ee069a3f
Dibeyendu Maity <dibeyendumaity@gmail.com>
2026-08-14 21:54:15 +0530

    register js added

---
+++ js/register.js
@@ -0,0 +1,17 @@
+// ==========================================
+// SELECT ELEMENTS
+// ==========================================
+
+const registerForm = document.getElementById("registerForm");
+
+const name = document.getElementById("name");
+
+const email = document.getElementById("email");
+
+const password = document.getElementById("password");
+
+const confirmPassword = document.getElementById("confirmPassword");
+
+const togglePassword = document.getElementById("togglePassword");
+
+const toggleConfirmPassword = document.getElementById("toggleConfirmPassword");
